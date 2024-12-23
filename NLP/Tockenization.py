13a643a999b2081ac5df182112aee624321bfd58
shawinCodes <sabinneupane31@gmail.com>
2024-12-23 18:40:47 +0545

    tokenization

--- NLP/Tockenization.py
+++ NLP/Tockenization.py
@@ -1,21 +1,15 @@
 import nltk
 from nltk.tokenize import word_tokenize, sent_tokenize
 
-def tokenization_example():
-    # Sample text
-    text = "Hello everyone. This is Sabin Neupane a BIT Student from BMC. I'm currently in 4th Semester"
+text = "Hello everyone. This is Sabin Neupane a BIT Student from BMC. I'm currently in 4th Semester"
 
-    # Perform word tokenization
-    word_tokens = word_tokenize(text)
+word_tokens = word_tokenize(text)
 
-    # Perform sentence tokenization
-    sent_tokens = sent_tokenize(text)
+sent_tokens = sent_tokenize(text)
 
-    # Print results
-    print("Word Tokens:")
-    print(word_tokens)
-    print("\nSentence Tokens:")
-    for sentence in sent_tokens:
-        print(sentence)
+print("Word Tokens:")
+print(word_tokens)
+print("\nSentence Tokens:")
+for sentence in sent_tokens:
+    print(sentence)
 
-tokenization_example()
